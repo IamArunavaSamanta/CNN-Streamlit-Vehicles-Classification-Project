4aba456622304cdc01b160f1ec10a04a57d92bd0
Arunava Samanta <arunavasamanta001@gmail.com>
2025-11-18 22:40:46 +0530

    Update StreamlitVehicle.py

--- StreamlitVehicle.py
+++ StreamlitVehicle.py
@@ -35,7 +35,7 @@ if "logged_in" not in st.session_state:
 if "user_name" not in st.session_state:
     st.session_state.user_name = ""
 # ✅File path for storing login details
-url = "https://raw.githubusercontent.com/IamArunavaSamanta/CNN-Streamlit-Vehicles-Classification-Project/main/Users%20Details.xlsx
+url = "https://raw.githubusercontent.com/IamArunavaSamanta/CNN-Streamlit-Vehicles-Classification-Project/main/Users%20Details.xlsx"
 df = pd.read_excel(url, engine='openpyxl')
 st.write(df.head())
 
@@ -321,6 +321,7 @@ else:
         if st.button("🚪Logout"):
             st.session_state.logged_in = False
             st.rerun()
+
 
 
 
