883d6dcf4b781fad688aae867b4197b8d1bc48c3
Arunava Samanta <arunavasamanta001@gmail.com>
2025-11-18 22:37:25 +0530

    Update StreamlitVehicle.py

--- StreamlitVehicle.py
+++ StreamlitVehicle.py
@@ -35,7 +35,7 @@ if "logged_in" not in st.session_state:
 if "user_name" not in st.session_state:
     st.session_state.user_name = ""
 # ✅File path for storing login details
-file_path = r"C:\Arunava Docs\Pyspark Resourse\ML\CNN\Vehicles\New Microsoft Excel Worksheet.xlsx"
+file_path = "https://github.com/IamArunavaSamanta/CNN-Streamlit-Vehicles-Classification-Project/blob/main/Users%20Details.xlsx"
 
 st.write('# :rainbow[Vehicle Classification]🔥')
 st.info("Vehicle classification is a computer vision task that uses machine learning to automatically identify and categorize vehicles\
@@ -318,6 +318,7 @@ else:
         if st.button("🚪Logout"):
             st.session_state.logged_in = False
             st.rerun()
+
 
 
 
