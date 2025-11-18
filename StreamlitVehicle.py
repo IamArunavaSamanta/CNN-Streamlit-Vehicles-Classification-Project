7add5a4c60f4d41db8bb7652a9171676747fe04b
Arunava Samanta <arunavasamanta001@gmail.com>
2025-11-18 23:28:34 +0530

    Update StreamlitVehicle.py

--- StreamlitVehicle.py
+++ StreamlitVehicle.py
@@ -35,6 +35,7 @@ if "logged_in" not in st.session_state:
 if "user_name" not in st.session_state:
     st.session_state.user_name = ""
 # ✅File path for storing login details
+st.write("Exists?", os.path.exists(r"C:\Users\ARSAMANT\Downloads"))
 url = r"C:\Users\ARSAMANT\Downloads\Users Details.csv"
 df = pd.read_csv(url)                              
 st.write(df.head())       
@@ -316,6 +317,7 @@ else:
         if st.button("🚪Logout"):
             st.session_state.logged_in = False
             st.rerun()
+
 
 
 
